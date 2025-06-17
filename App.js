a6967b07c677345002263862c2d68e8360312741
Norman Kipchirchir <normankipchirchir@gmail.com>
2025-06-17 14:29:04 +0300

    initial commit for ThePaye app

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to The PAYE App 🚀</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eef',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
